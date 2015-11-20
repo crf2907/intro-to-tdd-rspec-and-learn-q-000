df033d218e7b6ab6ff4db20bd91bf9abe29ec1dc
crf2907 <cfrancis275@googlemail.com>
2015-11-20 14:52:28 +0000

    nitrous-backup at Fri Nov 20 14:52:28 UTC 2015

---
+++ how_old_are_you.rb
@@ -0,0 +1,8 @@
+def greeting(name, language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting("Sophie", "Ember.js")
+# > Hello, Sophie. We heard you are a great Ember.js programmer. 
+
+greeting("Dan")
